5f6e8bf3c58de5208bbf467588a321297ca19ec0
Seeumt <Seeumt@cumt.edu.cn>
2020-04-30 01:05:50 +0800

    Hi~ This is Intelligence SM

---
+++ src/main/java/cn/edu/cumt/sm/IntelligenceApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.cumt.sm;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/cumt/sm/IntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.cumt.sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
